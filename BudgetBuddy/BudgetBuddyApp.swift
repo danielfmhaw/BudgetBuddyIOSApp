37a485909b6a1f0e17f6dc97b485ff522e159a6e
Daniel <danielfmhaw@gamil.com>
2023-04-29 14:19:16 +0200

    Initial Commit

---
+++ BudgetBuddy/BudgetBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetBuddyApp.swift
+//  BudgetBuddy
+//
+//  Created by Daniel Mendes on 29.04.23.
+//
+
+import SwiftUI
+
+@main
+struct BudgetBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
