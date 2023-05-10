a960b0d1f139a54f37be623f0c5fcc6dbe7b975a
Daniel <danielfmhaw@gamil.com>
2023-05-10 11:28:42 +0200

    Änderungen an dem BuddyChat

--- BudgetBuddy/Buddy.swift
+++ BudgetBuddy/Buddy.swift
@@ -19,85 +19,110 @@ struct Buddy: View {
     @State private var messagesComp: [String] = []
     
     
+    
     var body: some View {
-        NavigationView {
-            VStack {
-                Button(action: {
-                    messagesBenutzer = []
-                    messagesComp = []
-                    chatText = ""
-                }) {
-                    Label("Chat leeren", systemImage: "trash.fill")
-                }
-                
-                Text("Chat")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .padding(.top, 20)
-                
-                
-                ScrollView {
-                    LazyVStack(alignment: .leading, spacing: 10) {
-                        ForEach(Array(zip(messagesBenutzer, messagesComp)), id: \.0) { messageBenutzer, messageComp in
-                            VStack(alignment: .trailing, spacing: 5) {
-                                if messageBenutzer != "" {
-                                    HStack {
-                                        Spacer()
-                                        Text(messageBenutzer)
-                                            .foregroundColor(.white)
-                                            .padding(.all, 10)
-                                            .background(Color.blue)
-                                            .cornerRadius(15)
-                                    }
-                                }
-                                if messageComp != "" {
-                                    HStack {
-                                        Text(messageComp)
-                                            .foregroundColor(.black)
-                                            .padding(.all, 10)
-                                            .background(Color.gray.opacity(0.2))
-                                            .cornerRadius(15)
-                                        Spacer()
-                                    }
-                                }
-                            }
-                            .padding(.horizontal, 50)
-                        }
-                    }
-                }
+       NavigationView {
+           VStack {
+               Button(action: {
+                   messagesBenutzer = []
+                   messagesComp = []
+                   chatText = ""
+               }) {
+                   Label("Chat leeren", systemImage: "trash.fill")
+               }
+               
+               Text("Chat")
+                   .font(.title)
+                   .fontWeight(.bold)
+                   .padding(.top, 20)
+               
+               
+               ScrollView {
+                   ScrollViewReader { scrollView in
+                       LazyVStack(alignment: .leading, spacing: 10) {
+                           ForEach(Array(zip(messagesBenutzer, messagesComp)), id: \.0) { messageBenutzer, messageComp in
+                               VStack(alignment: .trailing, spacing: 5) {
+                                   if messageBenutzer != "" {
+                                       HStack {
+                                           Spacer()
+                                           Text(messageBenutzer)
+                                               .foregroundColor(.white)
+                                               .padding(.all, 10)
+                                               .background(Color.blue)
+                                               .cornerRadius(15)
+                                       }
+                                   }
+                                   if messageComp != "" {
+                                       HStack {
+                                           Text(messageComp)
+                                               .foregroundColor(.black)
+                                               .padding(.all, 10)
+                                               .background(Color.gray.opacity(0.2))
+                                               .cornerRadius(15)
+                                           Spacer()
+                                       }
+                                   }
+                               }
+                               .padding(.horizontal, 50)
+                               .id(messageComp)
+                           }
+                       }
+                       .onChange(of: messagesComp) { _ in
+                           withAnimation {
+                               scrollView.scrollTo(messagesComp.last, anchor: .bottom)
+                           }
+                       }
+                   }
+               }
 
 
-
-
-
-                HStack {
-                    TextField("Schreibe eine Nachricht", text: $chatText)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                    
-                    Button("Senden") {
-                        sendMessage(chatText)
-                        chatText = ""
-                    }
-                    .padding(.leading, 10)
-                }
-                .padding(.horizontal, 20)
-                .padding(.bottom, 10)
-            }
-        }
-        .onAppear {
-            fetchLimits()
-            fetchActivities()
-        }
+               HStack {
+                   TextField("Schreibe eine Nachricht", text: $chatText)
+                       .textFieldStyle(RoundedBorderTextFieldStyle())
+                   
+                   Button("Senden") {
+                       sendMessage(chatText)
+                       chatText = ""
+                   }
+                   .padding(.leading, 10)
+               }
+               .padding(.horizontal, 20)
+               .padding(.bottom, 10)
+           }
+       }
+       .onAppear {
+           fetchLimits()
+           fetchActivities()
+       }
     }
-    
-    
+       
     func sendMessage(_ message: String) {
         messagesBenutzer.append(message)
         if message.lowercased() == "hallo" || message.lowercased() == "hi" {
             messagesComp.append("Was kann ich für dich tun?")
         }else if message.lowercased() == "danke" || message.lowercased() == "thx" {
             messagesComp.append("Kein Problem ;)")
-        } else if message.lowercased().contains("lim"){
+        }else if message.lowercased().contains("einna"){
+            let einnahmenMessages = getBudgetInformation(art: "Einnahmen")
+            var alles: String = ""
+            for (index, message) in einnahmenMessages.enumerated() {
+                alles += "• " + message
+                if index < einnahmenMessages.count - 1 {
+                    alles += "\r\n"
+                }
+            }
+            messagesComp.append(alles)
+        }else if message.lowercased().contains("ausg"){
+            let ausgabenMessages = getBudgetInformation(art:"Ausgaben")
+            var alles: String = ""
+            for (index, message) in ausgabenMessages.enumerated() {
+                alles += "• " + message
+                if index < ausgabenMessages.count - 1 {
+                    alles += "\r\n"
+                }
+            }
+            messagesComp.append(alles)
+        }else if message.lowercased().contains("lim"){
             let limitMessages = getLimitInformation()
             var alles:String = ""
             for (index, limitMessage) in limitMessages.enumerated() {
@@ -111,7 +136,24 @@ struct Buddy: View {
             messagesComp.append("Entschuldigung, ich verstehe nicht, was du meinst.")
         }
     }
-
+    
+    func getBudgetInformation(art:String) -> [String] {
+        var messages: [String] = []
+        let budgetmessage = berechneNachKategorieUndArt(art: art)
+        for budget in budgetmessage  {
+            if(budget.anteil>40){
+                messages.append("Kategorie \(budget.kategorie) sehr großer Anteil (\(String(format: "%.2f", abs(budget.anteil)))%)")
+            }else if(budget.anteil>20){
+                messages.append("Kategorie \(budget.kategorie) großer Anteil (\(String(format: "%.2f", abs(budget.anteil)))%)")
+            }else if(budget.anteil>10){
+                messages.append("Kategorie \(budget.kategorie) wichtig (\(String(format: "%.2f", abs(budget.anteil)))%)")
+            }
+        }
+        if(messages.isEmpty){
+            messages.append("Keine Anteil der \(art) über 10%")
+        }
+        return messages
+    }
     
     func getLimitInformation() -> [String] {
         var messages: [String] = []
@@ -125,12 +167,31 @@ struct Buddy: View {
         return messages
     }
     
+    func berechneNachKategorieUndArt(art:String) -> [(kategorie: String, anteil: Double)] {
+        var nachKategorie: [(kategorie: String, summe: Double)] = []
+        var gesamtsumme: Double = 0
+        activities.filter { $0.art == art }.forEach { aktivitaet in
+            if let index = nachKategorie.firstIndex(where: { $0.kategorie == aktivitaet.kategorie }) {
+                nachKategorie[index].summe += aktivitaet.betrag
+                gesamtsumme+=aktivitaet.betrag
+            } else {
+                nachKategorie.append((kategorie: aktivitaet.kategorie, summe: aktivitaet.betrag))
+            }
+        }
+        var prozentual: [(kategorie: String, anteil: Double)] = []
+        nachKategorie.forEach{aktivitaet in
+            prozentual.append((kategorie: aktivitaet.kategorie, anteil: (aktivitaet.summe/gesamtsumme)*100))
+        }
+        return prozentual
+    }
+    
+    
     private func createLimitAnalysen() {
         let kategorien = Set(limits.map { $0.kategorie }) // Alle einzigartigen Kategorien
         
         var limitAnalysen = [LimitAnalyse]()
         for kategorie in kategorien {
-            let betragSumme = getAktivitaetenSummeInKategorie(kategorie: kategorie)
+            let betragSumme = getAktivitaetenSummeInKategorie(kategorie: kategorie,art: "Ausgaben")
             let limitBetrag = limits.filter { $0.kategorie == kategorie }.first?.betrag ?? 0
             limitAnalysen.append(LimitAnalyse(kategorie: kategorie, zielbetrag: limitBetrag, aktuell: betragSumme))
         }
@@ -138,8 +199,9 @@ struct Buddy: View {
         print(limitAnalysen)
     }
     
-    private func getAktivitaetenSummeInKategorie(kategorie: String) -> Double {
-        let aktivitaetenInKategorie = activities.filter { $0.kategorie == kategorie }
+    private func getAktivitaetenSummeInKategorie(kategorie: String,art:String) -> Double {
+        let aktivitaetenAusgaben = activities.filter { $0.art == art }
+        let aktivitaetenInKategorie = aktivitaetenAusgaben.filter { $0.kategorie == kategorie }
         let betragSumme = aktivitaetenInKategorie.reduce(0) { $0 + $1.betrag }
         return betragSumme
     }
@@ -168,7 +230,7 @@ struct Buddy: View {
     
     
     private func fetchActivities() {
-        guard let url = URL(string: "http://localhost:8080/api/v1/aktivitaet/withArt/\(email)/Ausgaben") else {
+        guard let url = URL(string: "http://localhost:8080/api/v1/aktivitaet/\(email)") else {
             print("Invalid URL")
             return
         }
